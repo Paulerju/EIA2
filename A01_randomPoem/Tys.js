0b783f948dec3267c55129700e07955918cdad97
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-03-31 12:08:11 +0200

    update

--- A01_randomPoem/Tys.js
+++ A01_randomPoem/Tys.js
@@ -4,8 +4,8 @@ var Poem;
     /*
     Aufgabe: <Aufgabe 1 RandomPoem>
     Name: <Judith Pauler>
-    Matrikel: <Deine Matrikelnummer>
-    Datum: <Datum der letzten Bearbeitung>
+    Matrikel: <272240>
+    Datum: <24.03.23>
     Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
     */
     let subjekte = ["Bellatrix", "Rodolphus", "Lucius", "James", "Snape", "Sirius"];
@@ -18,19 +18,32 @@ var Poem;
     // console.log(praedikate);
     // console.log(objekte);
     for (let i = subjekte.length; i > 0; i--) {
-        i = i;
         // console.log(i);
-        getVerse(subjekte, praedikate, objekte);
+        console.log(getVerse(subjekte, praedikate, objekte));
     }
     function getVerse(_a, _b, _c) {
         let vers = "";
         let count = Math.random() * _a.length;
         count = Math.floor(count);
+<<<<<<< HEAD
         console.log(count);
         _a.splice(count);
         console.log(vers);
         let str = _a[1];
         return str;
+=======
+        vers = vers + _a[count] + " ";
+        _a.splice(count, 1);
+        let count2 = Math.random() * _b.length;
+        count2 = Math.floor(count2);
+        vers = vers + _b[count2] + " ";
+        _b.splice(count2, 1);
+        let count3 = Math.random() * _c.length;
+        count3 = Math.floor(count3);
+        vers = vers + _c[count3] + " ";
+        _c.splice(count3, 1);
+        return vers;
+>>>>>>> bb81f380635539259241649308202849f340d417
     }
     // console.log(getVerse(subjekte, praedikate, objekte)); 
 })(Poem || (Poem = {}));
