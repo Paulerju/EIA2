377d8d3201d3a8e5bb3a9e110a45970dc3810bbc
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-04-07 19:37:40 +0200

    html

---
+++ A03_ToDoApp/main.ts
@@ -0,0 +1,15 @@
+document.querySelector("#bye")?.addEventListener("click",function(){
+
+});
+
+document.querySelector("#add")?.addEventListener("click",function(){
+
+});
+
+document.querySelector("#add2")?.addEventListener("click",function(){
+
+});
+
+document.querySelector("#box")?.addEventListener("click",function(){
+
+});
