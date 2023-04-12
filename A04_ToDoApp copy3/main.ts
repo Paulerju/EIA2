c81e91501098d0f27a797aaaf03008393bc4ca33
Paulerju <judith.anna.pauler@hs-furtwangen.de>
2023-04-12 17:41:19 +0200

    A4

---
+++ A04_ToDoApp copy3/main.ts
@@ -0,0 +1,30 @@
+namespace ToDO2 {
+    /*
+      Aufgabe: <Aufgabe 3 ToDo>
+      Name: <Judith Pauler>
+      Matrikel: <272240>
+      Datum: <08.04.23>
+      Quellen: <>
+      */
+
+  let wrap = <HTMLElement> document.querySelector("#wrapper"); 
+
+document.querySelector("#bye")?.addEventListener("click",function(){
+
+});
+
+document.querySelector("#add")?.addEventListener("click",function(){
+  wrap.style.setProperty("visibility", "visible"); 
+
+});
+
+document.querySelector("#add2")?.addEventListener("click",function(){
+  wrap.style.setProperty("visibility", "hidden"); 
+  
+
+});
+
+document.querySelector("#box")?.addEventListener("click",function(){
+
+});
+}
