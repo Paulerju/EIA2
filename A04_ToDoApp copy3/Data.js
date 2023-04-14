bfe6d120962f5430903d82bfe1998d71e3abc458
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-04-14 19:46:37 +0200

    better

--- A04_ToDoApp copy3/Data.js
+++ A04_ToDoApp copy3/Data.js
@@ -10,13 +10,8 @@ var Datensammlung;
  
      } */
     let taskArray1 = [];
+    let add = document.querySelector("#add2");
     let strArray1 = taskArray1.toString();
-    let newdiv = document.createElement("div");
-    newdiv.setAttribute("id", "newtask");
-    let newP = document.createElement("p");
-    document.getElementById("div1").appendChild(newdiv);
-    document.querySelector("#div1").appendChild(newP);
-    newP.innerHTML = "Whatttttttttttttt isssssssssssssss gooooing on";
     function getData() {
         const form = document.querySelector('#form1');
         let taskArray;
@@ -30,10 +25,11 @@ var Datensammlung;
         taskArray = [text0, text1, text2, text3, text4];
         console.log("getData: " + taskArray);
         taskArray1 = taskArray; // why isnt this working?
-        console.log("in: " + taskArray1);
         taskArray1 = taskArray;
+        console.log("in: " + taskArray1);
         return taskArray1;
     }
+    ;
     let wrap = document.querySelector("#wrapper");
     // let div1 = document.querySelector("#task1");
     let Trashbin = document.createElement("button");
@@ -45,10 +41,22 @@ var Datensammlung;
     document.querySelector("#add").addEventListener("click", function () {
         wrap.style.setProperty("visibility", "visible");
     });
-    document.querySelector("#add2").addEventListener("click", function () {
+    function ArraytoString() {
+        strArray1 = taskArray1.toString();
+    }
+    ;
+    document.querySelector("#add2").addEventListener("click", function (e) {
         // wrap.style.setProperty("visibility", "hidden"); 
         getData();
-        console.log("out: " + taskArray1);
+        ArraytoString();
+        console.log("out: " + strArray1);
+        let newdiv = document.createElement("div");
+        newdiv.setAttribute("id", "newtask");
+        let newP = document.createElement("p");
+        document.getElementById("div1").appendChild(newdiv);
+        document.querySelector("#div1").appendChild(newP);
+        newP.innerHTML = strArray1;
+        e.preventDefault();
     });
     document.querySelector("#box")?.addEventListener("click", function () {
     });
--- A04_ToDoApp copy3/Data.ts
+++ A04_ToDoApp copy3/Data.ts
@@ -10,14 +10,10 @@ namespace Datensammlung {
      } */
 
     let taskArray1: String[] = [];
+    let add = document.querySelector("#add2");
     let strArray1: string = taskArray1.toString();
-    let newdiv = document.createElement("div");
-    newdiv.setAttribute("id", "newtask");
-    let newP = document.createElement("p"); 
-    document.getElementById("div1")!.appendChild(newdiv);
-    document.querySelector("#div1")!.appendChild(newP);
-    newP.innerHTML = "Whatttttttttttttt isssssssssssssss gooooing on";
     
+     
 
 
     function getData(): String[] {
@@ -36,11 +32,11 @@ namespace Datensammlung {
         taskArray = [text0, text1, text2, text3, text4];
         console.log("getData: " + taskArray);
         taskArray1 = taskArray; // why isnt this working?
-        console.log("in: " + taskArray1);
+       
         taskArray1 = taskArray;
-
+        console.log("in: " + taskArray1);
         return taskArray1;
-    }
+    }; 
 
     let wrap = <HTMLElement>document.querySelector("#wrapper");
     // let div1 = document.querySelector("#task1");
@@ -58,11 +54,23 @@ namespace Datensammlung {
 
     });
 
-    document.querySelector("#add2")!.addEventListener("click", function () {
+    function ArraytoString(){
+        strArray1 = taskArray1.toString();
+    };
+
+    
+    document.querySelector("#add2")!.addEventListener("click", function (e) {
         // wrap.style.setProperty("visibility", "hidden"); 
-        getData();
-     
-        console.log("out: " + taskArray1);
+      getData();
+      ArraytoString();
+      console.log("out: " + strArray1)
+      let newdiv = document.createElement("div");
+      newdiv.setAttribute("id", "newtask");
+      let newP = document.createElement("p"); 
+      document.getElementById("div1")!.appendChild(newdiv);
+      document.querySelector("#div1")!.appendChild(newP);
+      newP.innerHTML = strArray1;
+      e.preventDefault();
 
     });
 
