c6a97b8f16ea9e2821e22e836a4ac3b1d5ae60b1
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-04-14 11:36:06 +0200

    form/ fieldset

--- A04_ToDoApp copy3/Data.js
+++ A04_ToDoApp copy3/Data.js
@@ -1,9 +1,17 @@
 "use strict";
 var Datensammlung;
 (function (Datensammlung) {
-    let formData1 = new FormData(document.forms[0]);
-    let data = {
-        taskname: , date: "17.04.2023", comment: "nicht vergessen das zu machen....", person: [], progress: true
+    let form = document.querySelector("#from1");
+    form.onsubmit = () => {
+        const formData = new FormData(form);
+        console.log(formData);
+        const text = formData.get('textInput');
+        console.log(text);
+        return false; // prevent reload
     };
+    /*
+        let data: Aufgabe = {
+            taskname: , date: "17.04.2023", comment: "nicht vergessen das zu machen....", person: [], progress: true
+        };*/
 })(Datensammlung || (Datensammlung = {}));
 //# sourceMappingURL=Data.js.map
--- A04_ToDoApp copy3/Data.ts
+++ A04_ToDoApp copy3/Data.ts
@@ -1,18 +1,28 @@
 namespace Datensammlung {
 
-   export interface Aufgabe {
-        taskname: string; 
-        date: string; 
+    export interface Aufgabe {
+        taskname: string;
+        date: string;
         comment: string;
-        person: Selection[]; 
-        progress: boolean; 
-    
+        person: Selection[];
+        progress: boolean;
+
     }
 
-    let formData1: FormData = new FormData(document.forms[0]);
+    let form = <HTMLFormElement>document.querySelector("#from1");
+    
+    form.onsubmit = () => {
+        const formData = new FormData(form);
+      
+        console.log(formData);
+        const text = formData.get('textInput') as string;
+        console.log(text);
+        return false; // prevent reload
+      };
 
 
+/*
     let data: Aufgabe = {
-             taskname: , date: "17.04.2023" , comment: "nicht vergessen das zu machen....",person: [] , progress: true  
-    }; 
+        taskname: , date: "17.04.2023", comment: "nicht vergessen das zu machen....", person: [], progress: true
+    };*/
 }
