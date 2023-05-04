b725ceb340781d9f360b463976531024c407c7ec
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-05-04 12:58:11 +0200

    sun and bubbles

--- A08_Canvas/Canvas.js
+++ A08_Canvas/Canvas.js
@@ -1,17 +1,17 @@
 "use strict";
 var Canvas;
-(function (Canvas_1) {
+(function (Canvas) {
     window.addEventListener("load", handleLoad);
     let crc2;
-    drawT2(50, 20);
     function handleLoad(_event) {
-        let Canvas = document.querySelector("canvas");
-        if (!Canvas)
-            return;
-        crc2 = Canvas.getContext("2d");
+        let canvas = document.querySelector("canvas");
+        // if (!Canvas)
+        console.log("canvas not working");
+        //return; 
+        crc2 = canvas.getContext("2d");
         drawBackground();
-        drawT1({ x: 50, y: 50 });
-        drawT2({ x: 500, y: 125 }, { x: 250, y: 75 });
+        drawT1({ x: 50, y: 80 });
+        drawT2({ x: 500, y: 125 }, { x: 550, y: -110 });
     }
     function drawBackground() {
         console.log("Background");
@@ -30,27 +30,31 @@ var Canvas;
         gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
         crc2.save();
         crc2.fillStyle = gradient;
+        crc2.translate(_position.x, _position.y);
         crc2.arc(0, 0, r2, 0, 2 * Math.PI);
         crc2.fill();
         crc2.restore();
     }
     function drawT2(_position, _size) {
         console.log("T2");
-        let nParticles = 15;
+        let nParticles = 10;
         let particle = new Path2D();
-        let radiusParticle = 20;
+        let radiusParticle = 8;
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "HSLA(0,100%,100%,0.5)");
-        gradient.addColorStop(1, "HSLA(0,100%,100%,0)");
+        gradient.addColorStop(0, "lightblue");
+        gradient.addColorStop(1, "blue");
+        crc2.fillStyle = gradient;
         crc2.save();
         crc2.translate(_position.x, _position.y);
-        crc2.fillStyle = gradient;
+        crc2.restore();
         for (let drawn = 0; drawn < nParticles; drawn++) {
             let x = (Math.random() - 0.5) * _size.x;
             let y = -(Math.random() * _size.y);
+            crc2.save();
             crc2.translate(x, y);
             crc2.fill(particle);
+            crc2.restore();
         }
     }
 })(Canvas || (Canvas = {}));
--- A08_Canvas/Canvas.ts
+++ A08_Canvas/Canvas.ts
@@ -6,17 +6,17 @@ namespace Canvas {
 
 window.addEventListener("load", handleLoad);
 let crc2: CanvasRenderingContext2D; 
-drawT2(50,20);
 
 function handleLoad(_event:Event):void{
-    let Canvas: HTMLCanvasElement | null = document.querySelector("canvas");
-    if (!Canvas)
-    return; 
-    crc2 = <CanvasRenderingContext2D>Canvas.getContext("2d");
+    let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+   // if (!Canvas)
+    console.log("canvas not working");
+    //return; 
+    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
 
     drawBackground();
-    drawT1({x: 50, y:50}); 
-    drawT2({x: 500, y:125}, {x: 250, y: 75}); 
+    drawT1({x: 50, y: 80}); 
+    drawT2({x: 500, y:125}, {x: 550, y: -110}); 
 }
 
 function drawBackground():void {
@@ -35,37 +35,41 @@ function drawT1(_position: Vector):void {
     let r1: number = 20; 
     let r2: number = 40; 
     let gradient: CanvasGradient = crc2.createRadialGradient(0,0,r1,0,0,r2);
-
     gradient.addColorStop(0, "yellow");
     gradient.addColorStop(1,"HSLA(60, 100%, 50%, 0)");
 
     crc2.save();
     crc2.fillStyle = gradient; 
+    crc2.translate(_position.x, _position.y);
     crc2.arc(0,0,r2,0,2*Math.PI);
     crc2.fill();
     crc2.restore(); 
+    
 
 }
 function drawT2(_position: Vector, _size: Vector):void{
     console.log("T2");
-    let nParticles: number = 15;
+    let nParticles: number = 10;
     let particle: Path2D = new Path2D();
-    let radiusParticle: number = 20; 
+    let radiusParticle: number = 8; 
     let gradient: CanvasGradient = crc2.createRadialGradient(0,0,0,0,0,radiusParticle);
 
     particle.arc(0,0,radiusParticle,0,2*Math.PI);
-    gradient.addColorStop(0,"HSLA(0,100%,100%,0.5)");
-    gradient.addColorStop(1,"HSLA(0,100%,100%,0)");
+    gradient.addColorStop(0,"lightblue");
+    gradient.addColorStop(1,"blue"); 
+    crc2.fillStyle = gradient;
 
     crc2.save();  
     crc2.translate(_position.x, _position.y);
-    crc2.fillStyle = gradient; 
+    crc2.restore(); 
 
     for (let drawn: number = 0; drawn < nParticles; drawn++){
     let x: number = (Math.random() - 0.5)* _size.x; 
     let y: number = - (Math.random() * _size.y);
+    crc2.save();
     crc2.translate(x,y);    
     crc2.fill(particle);
+    crc2.restore(); 
 
     }
 }
