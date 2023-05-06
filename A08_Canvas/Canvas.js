e95c93174e7a4318e5c6178e005d43820fd34877
Paulerju <101625370+Paulerju@users.noreply.github.com>
2023-05-06 18:06:03 +0200

    A08

--- A08_Canvas/Canvas.js
+++ A08_Canvas/Canvas.js
@@ -10,8 +10,11 @@ var Canvas;
         //return; 
         crc2 = canvas.getContext("2d");
         drawBackground();
-        drawT1({ x: 50, y: 80 });
+        drawCircle({ x: -200, y: 150 }, { x: 70, y: 210 });
         drawT2({ x: 500, y: 125 }, { x: 550, y: -110 });
+        drawElipse();
+        putText();
+        drawT1();
     }
     function drawBackground() {
         console.log("Background");
@@ -20,30 +23,21 @@ var Canvas;
         gradient.addColorStop(1, "white");
         crc2.fillStyle = gradient;
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-    }
-    function drawT1(_position) {
-        console.log("T1");
-        let r1 = 20;
-        let r2 = 40;
-        let gradient = crc2.createRadialGradient(0, 0, r1, 0, 0, r2);
-        gradient.addColorStop(0, "yellow");
-        gradient.addColorStop(1, "HSLA(60, 100%, 50%, 0)");
-        crc2.save();
-        crc2.fillStyle = gradient;
-        crc2.translate(_position.x, _position.y);
-        crc2.arc(0, 0, r2, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.restore();
+        let x = Math.floor(Math.random() * (350 - 20 + 1) + 20);
+        let y = Math.floor(Math.random() * (200 - 20 + 1) + 20);
+        crc2.clearRect(x, 50, 25, 25);
+        crc2.strokeRect(250, y, 10, 10);
     }
     function drawT2(_position, _size) {
         console.log("T2");
-        let nParticles = 10;
+        let nParticles = 40;
         let particle = new Path2D();
         let radiusParticle = 8;
         let gradient = crc2.createRadialGradient(0, 0, 0, 0, 0, radiusParticle);
         particle.arc(0, 0, radiusParticle, 0, 2 * Math.PI);
-        gradient.addColorStop(0, "lightblue");
-        gradient.addColorStop(1, "blue");
+        gradient.addColorStop(1, "lightblue");
+        gradient.addColorStop(0, "HSLA(60, 100%, 50%, 0)");
+        gradient.addColorStop(0.5, "HSLA(60, 100%, 50%, 0)");
         crc2.fillStyle = gradient;
         crc2.save();
         crc2.translate(_position.x, _position.y);
@@ -56,6 +50,59 @@ var Canvas;
             crc2.fill(particle);
             crc2.restore();
         }
+    }
+    ;
+    function drawElipse() {
+        console.log("T3");
+        let a = Math.floor(Math.random() * 20);
+        let b = Math.floor(Math.random() * 40);
+        crc2.beginPath();
+        crc2.ellipse(230, 80, a, b, Math.PI / 6, 0, 2 * Math.PI);
+        crc2.stroke();
+        crc2.closePath();
+        crc2.beginPath();
+        crc2.moveTo(0, 200);
+        crc2.lineTo(500, 300);
+        crc2.stroke();
+        //  crc2.fillStyle = crc2.createPattern(crc2.canvas, 'repeat')!;
+    }
+    function drawCircle(_position, _size) {
+        for (let i = 0; i < 500; i++) {
+            var x = Math.floor(Math.random() * _size.x);
+            let y = Math.floor(Math.random() * _size.y);
+            let radius = Math.floor(Math.random() * 15);
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            crc2.beginPath();
+            crc2.arc(x, y, radius, Math.PI * 2, 0, false);
+            crc2.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
+            crc2.fill();
+            crc2.closePath();
+        }
+    }
+    function putText() {
+        crc2.fillText("Hello there", 50, 90);
+        crc2.strokeText("...here we are again", 100, 100);
+    }
+    function drawT1() {
+        console.log("T1");
+        var x = Math.floor(Math.random() * 250);
+        let y = Math.floor(Math.random() * 150);
+        let o = Math.floor(Math.random() * (350 - 20 + 1) + 20);
+        let one = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+        let two = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+        crc2.setTransform(1, 0.2, 0.8, 1, 0, 0);
+        crc2.fillRect(x, y, 40, 40);
+        let storedTransform = crc2.getTransform();
+        crc2.setTransform(storedTransform);
+        crc2.beginPath();
+        crc2.rotate(o * Math.PI / 180);
+        crc2.scale(one, two);
+        crc2.fillRect(x, y, 40, 40);
+        crc2.resetTransform();
+        crc2.closePath();
+        crc2.stroke();
     }
 })(Canvas || (Canvas = {}));
 //# sourceMappingURL=Canvas.js.map
