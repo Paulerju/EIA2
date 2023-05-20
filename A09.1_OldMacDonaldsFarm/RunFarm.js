5c593ac50bd1be7cdca3f908b7a5d25d4226968e
Paulerju <judith.anna.pauler@hs-furtwangen.de>
2023-05-20 16:29:44 +0200

    Update Animal class

--- A09.1_OldMacDonaldsFarm/RunFarm.js
+++ A09.1_OldMacDonaldsFarm/RunFarm.js
@@ -4,11 +4,11 @@ var OMDF;
     window.addEventListener("load", handleLoad);
     let canvas = document.querySelector("canvas");
     OMDF.crc2 = canvas.getContext("2d");
-    let dog = new OMDF.Dog();
-    let cow = new OMDF.Cow();
-    let pig = new OMDF.Pig();
-    let chicken = new OMDF.Chicken();
-    let donkey = new OMDF.Donkey();
+    let chicken = new OMDF.Animal("chicken", "bock", "yellow", 1);
+    let cow = new OMDF.Animal("cow", "muh", "grass", 3);
+    let dog = new OMDF.Animal("dog", "wuf", "meat", 2);
+    let donkey = new OMDF.Animal("donkey", "ehah", "stroh", 2);
+    let pig = new OMDF.Animal("pig", "oink", "brown", 3);
     let food = new OMDF.Food(10, OMDF.crc2);
     let foodDog = new OMDF.Food(10, OMDF.crc2);
     let foodPig = new OMDF.Food(10, OMDF.crc2);
@@ -17,11 +17,11 @@ var OMDF;
     let foodDonkey = new OMDF.Food(10, OMDF.crc2);
     function handleLoad(_event) {
         drawBackground();
-        dog.drawDog();
-        cow.drawCow();
-        pig.drawPig();
         chicken.drawChicken();
+        cow.drawCow();
+        dog.drawDog();
         donkey.drawDonkey();
+        pig.drawPig();
         drawWall();
         food.drawManger();
         foodDog.drawFood(3, 4, 5, 6, "#7d6f4f", "#8a7850", "#a39167", "#b59f6d");
@@ -29,25 +29,7 @@ var OMDF;
         foodCow.drawFood(41, 42, 43, 44, "#6e5b46", "#4a3926", "#362818", "#574531");
         foodChicken.drawFood(61, 62, 63, 64, "#c7a22a", "#c7a228", "#d1aa2a", "#c79f1e");
         foodDonkey.drawFood(82, 83, 84, 85, "#93a646", "#8a9950", "#7e8a4e", "#778a2b");
-        typeText("Old Mac Donalds had a farm, and on his farm he had some cows, with a ", () => {
-            cow.talk("moo-moo", () => {
-                typeText(" here and a ", () => {
-                    cow.talk("moo-moo", () => {
-                        typeText(" there. Here a ", () => {
-                            cow.talk("mo", () => {
-                                typeText(" there a ", () => {
-                                    cow.talk("mo", () => {
-                                        typeText(" Everywhere a moo-moo");
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        });
     }
-    ;
     function drawBackground() {
         let gradient = OMDF.crc2.createLinearGradient(0, 0, 0, OMDF.crc2.canvas.height);
         gradient.addColorStop(0, "lightblue");
@@ -59,25 +41,6 @@ var OMDF;
     function drawWall() {
         OMDF.crc2.fillStyle = "#262626";
         OMDF.crc2.fillRect(0, 290, OMDF.crc2.canvas.width, 250);
-    }
-    function typeText(_text, callback) {
-        const text = _text;
-        const delay = 100;
-        let currentCharIndex = 0;
-        function drawText() {
-            OMDF.crc2.fillText(text.substring(0, currentCharIndex), 10, 50);
-        }
-        function typeNextChar() {
-            if (currentCharIndex < text.length) {
-                drawText();
-                currentCharIndex++;
-                setTimeout(typeNextChar, delay);
-                if (callback) {
-                    callback();
-                }
-            }
-        }
-        typeNextChar();
     }
 })(OMDF || (OMDF = {}));
 //# sourceMappingURL=RunFarm.js.map
--- A09.1_OldMacDonaldsFarm/RunFarm.ts
+++ A09.1_OldMacDonaldsFarm/RunFarm.ts
@@ -3,47 +3,31 @@ namespace OMDF{
    export let crc2: CanvasRenderingContext2D;
    let canvas: HTMLCanvasElement = document.querySelector("canvas")!;
    crc2 = <CanvasRenderingContext2D>canvas.getContext("2d");
-   let dog= new Dog();
-   let cow= new Cow();
-   let pig = new Pig();
-   let chicken = new Chicken();
-   let donkey = new Donkey();
+    let chicken = new Animal("chicken","bock","yellow",1);
+    let cow = new Animal("cow","muh","grass",3);
+    let dog = new Animal("dog","wuf","meat",2);
+    let donkey = new Animal("donkey","ehah","stroh",2);
+    let pig = new Animal("pig","oink","brown",3);
+
    let food = new Food(10, crc2); let foodDog = new Food(10, crc2); let foodPig = new Food(10, crc2); let foodCow = new Food(10, crc2); let foodChicken = new Food(10, crc2);
    let foodDonkey = new Food(10, crc2); 
    
 
     function handleLoad(_event: Event): void {
         drawBackground();
-        dog.drawDog();
-        cow.drawCow();
-        pig.drawPig();
         chicken.drawChicken();
+        cow.drawCow();
+        dog.drawDog();
         donkey.drawDonkey();
+        pig.drawPig();
         drawWall();
         food.drawManger();
         foodDog.drawFood(3,4,5,6,"#7d6f4f","#8a7850","#a39167","#b59f6d");
         foodPig.drawFood(22,23,24,25,"#1d4727", "#1a4f27","#155224","#31633d");
         foodCow.drawFood(41,42,43,44,"#6e5b46","#4a3926","#362818","#574531");
         foodChicken.drawFood(61,62,63,64, "#c7a22a","#c7a228","#d1aa2a","#c79f1e");
-        foodDonkey.drawFood(82,83,84,85, "#93a646","#8a9950","#7e8a4e","#778a2b");
-        typeText("Old Mac Donalds had a farm, and on his farm he had some cows, with a ", () => {
-            cow.talk("moo-moo", () => {
-              typeText(" here and a ", () => {
-                cow.talk("moo-moo", () => {
-                  typeText(" there. Here a ", () => {
-                    cow.talk("mo", () => {
-                      typeText(" there a ", () => {
-                        cow.talk("mo", () => {
-                          typeText(" Everywhere a moo-moo");
-                        });
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        };
+        foodDonkey.drawFood(82,83,84,85, "#93a646","#8a9950","#7e8a4e","#778a2b"); 
+      }
    
 
     function drawBackground(): void {
@@ -61,28 +45,7 @@ namespace OMDF{
         crc2.fillStyle = "#262626";
         crc2.fillRect(0, 290, crc2.canvas.width, 250);
     }
-   
-  function typeText (_text:string, callback?:()=> void) {
-    const text = _text; 
-    const delay = 100; 
-    let currentCharIndex = 0;
-
-    function drawText() {
-        crc2.fillText(text.substring(0, currentCharIndex), 10, 50);
-      }
-    
-      function typeNextChar() {
-        if (currentCharIndex < text.length) {
-          drawText();
-          currentCharIndex++;
-          setTimeout(typeNextChar, delay);
-          if (callback) {
-            callback();
-          }
-        }
-      }
-      typeNextChar();
-  }
+  
 }
 
 
